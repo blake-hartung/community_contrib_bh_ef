925c15b024130259eb657f40b5e1422a672b1c3e
elijahflomen <elijah@u.northwestern.edu>
2021-10-29 14:55:18 -0400

    elijah commit

--- elijah.py
+++ elijah.py
@@ -9,7 +9,7 @@ import numpy as np
 
 
 
-st.title("Geo-Data Dataset Tutorial")
+st.title("Geo-Data Streamlit Tutorial")
 
 uber_files = ['/Users/elijahflomen/Downloads/archive/uber-raw-data-apr14.csv']
 
@@ -18,8 +18,10 @@ for file in uber_files:
     df = pd.read_csv(file,encoding='utf-8')
     ubers_df = pd.concat([df,ubers_df])
 
-# Only take 40% of the data to speed up visualization
-ubers_df = ubers_df.sample(frac=0.1)    
+# Allow user to select frac of data to analyze
+st.caption("Streamlit allows for the user to customize their EDAV experience")
+user_frac=st.slider("fraction of data", min_value=0.0, max_value=1.0)
+ubers_df = ubers_df.sample(frac=user_frac)    
 ubers_df = ubers_df.drop(columns=['Base'])
 ubers_df = ubers_df.rename(columns={"Lat": "lat", "Lon":"lon", "Date/Time": "Date"})
 
@@ -46,39 +48,32 @@ st.subheader("Map of Pickups From Calendar Date")
 st.map(ubers_df.loc[ubers_df['Date'] == user_cal_date])
 
 # Slider for categorical data with natural ordering...
-
 st.subheader("Map of Pickups From A Given Day")
 user_day_input = st.select_slider(label='Pick A Day', options=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
 st.map(ubers_df.loc[ubers_df['Day'] == user_day_input])
 
-# st.subheader("Heat Map for UK Accidents")
 
-# UK_ACCIDENTS_DATA = 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv'
+# Better visualizaiton for this type of dataset: 
+st.subheader("Heat Map for Uber Pickups")
+layers=[pdk.Layer(
+    "HexagonLayer",
+    data=ubers_df[["lon", "lat"]],
+    get_position=["lon", "lat"],
+    radius=100,
+    elevation_scale=4,
+    elevation_range=[0, 1000],
+    pickable=True,
+    extruded=True,
+    ),]
 
-# layer = pdk.Layer(
-#     'HexagonLayer', 
-#     UK_ACCIDENTS_DATA,
-#     get_position=['lng', 'lat'],
-#     auto_highlight=True,
-#     elevation_scale=50,
-#     pickable=True,
-#     elevation_range=[0, 3000],
-#     extruded=True,
-#     coverage=1)
+initial_view_state={
+    "latitude": ubers_df['lat'].mean(),
+    "longitude": ubers_df['lon'].mean(),
+    "zoom": 12,
+    'pitch': 50}
 
-# # Set the viewport location
-# view_state = pdk.ViewState(
-#     longitude=-1.415,
-#     latitude=52.2323,
-#     zoom=6,
-#     min_zoom=5,
-#     max_zoom=15,
-#     pitch=40.5,
-#     bearing=-27.36)
-
-# # Combined all of it and render a viewport
-# r = pdk.Deck(layers=[layer], initial_view_state=view_state)
-# st.pydeck_chart(r)
+st.pydeck_chart(pdk.Deck(layers=[layers], initial_view_state=initial_view_state))
+        
 
 
 
